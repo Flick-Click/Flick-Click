15d92d76339e2a55efb254ff742029db9b41f6cd
William Sundgård Anker <will1331@edu.sde.dk>
2021-01-27 13:03:18 +0100

    Creating the necessary Controllers

---
+++ Flick Click/Controllers/MostCommetedMovieController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Flick_Click.Controllers
+{
+    public class MostCommetedMovieController : Controller
+    {
+        // GET: MostCommetedMovie
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Flick Click/Controllers/VarifyAccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Flick_Click.Controllers
+{
+    public class VarifyAccountController : Controller
+    {
+        // GET: VarifyAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
