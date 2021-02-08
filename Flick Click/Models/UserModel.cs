5697311a771b6ca34857dcda37a36a1c2b3b1c83
Noah Kühne <noah0936@edu.sde.dk>
2021-02-08 14:01:52 +0100

    add a signUp side

---
+++ Flick Click/Models/UserModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flick_Click.Models
+{
+    public class UserModel
+    {
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "You need to give us your first name.")]
+        public string FistName { get; set; }
+
+        [Display(Name = "LastName")]
+        [Required(ErrorMessage = "You need to give us your last name.")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "You need to give us your Email Address.")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Confirm Email")]
+        [DataType(DataType.EmailAddress)]
+        [Compare("EmailAddress", ErrorMessage = "The email and Confirm Email must match.")]
+        public string ConfirmEmail { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 7, ErrorMessage = "You need to provide a long enough password.")]
+        [Required(ErrorMessage = "You need to give us your first name")]
+        public string Password { get; set; }
+
+        [Display(Name = "ConfirmPassword")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 7, ErrorMessage = "You need to provide a long enough password.")]
+        [Compare("Password", ErrorMessage = "The Password and Confirm Password must match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "TelefonNummer")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(15, MinimumLength = 7, ErrorMessage = "You need to provide a long enough TelefonNummer.")]
+        public string TelefonNummer { get; set; }
+    }
+}
--- FlickClick_ClassLibary/Models/UserModel.cs
+++ FlickClick_ClassLibary/Models/UserModel.cs
@@ -8,5 +8,14 @@ namespace FlickClick_ClassLibary.Models
 {
     class UserModel
     {
+
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string ConfirmEmail { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string TelefonNummer { get; set; }
+        public bool IsManager { get; set; } = false;
     }
 }
