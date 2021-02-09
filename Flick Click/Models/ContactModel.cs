c37c3b06ddc0d78f8bac1702bd3b3bbd4c72d117
William Sundgård Anker <will1331@edu.sde.dk>
2021-02-09 15:12:30 +0100

    made contactModel

---
+++ Flick Click/Models/ContactModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Flick_Click.Models
+{
+    public class ContactModel
+    {
+        [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "You need to write you full name")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Email Address:")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "You need to give us a email so we can write back")]
+        public string EmailAddress { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(1000, MinimumLength = 30, ErrorMessage = "Write an actual sentence")]
+        [Required(ErrorMessage = "It is Probably a good Idea too write a messege if you want to contact us")]
+        public string Messege { get; set; }
+    }
+}
