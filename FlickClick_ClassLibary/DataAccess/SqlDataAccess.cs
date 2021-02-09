c5b1d7da989db65bb3486fd0fefe70357788e4e9
Tim Sommerstedt <timx1167@edu.sde.dk>
2021-02-09 11:21:43 +0100

    Database connection

--- FlickClick_ClassLibary/DataAccess/SqlDataAccess.cs
+++ FlickClick_ClassLibary/DataAccess/SqlDataAccess.cs
@@ -7,19 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Dapper;
+using MySqlConnector;
 
 namespace FlickClick_ClassLibary.DataAccess
 {
     public static class SqlDataAccess
     {
-        public static string GetConnectionString(string connectionName = "Flick_Click")
+        public static string GetConnectionString(string connectionName = "server=localhost;user id=root;Pwd=tim;database=flick_click;persistsecurityinfo=True; allowuservariables=True")
         {
-            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+            return connectionName;
         }
-        
+
         public static List<T> LoadData<T>(string sql)
         {
-            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            using (IDbConnection cnn = new MySqlConnection(GetConnectionString()))
             {
                 return cnn.Query<T>(sql).ToList();
             }
@@ -27,7 +28,7 @@ namespace FlickClick_ClassLibary.DataAccess
 
         public static int SavaData<T>(string sql, T data)
         {
-            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            using (IDbConnection cnn = new MySqlConnection(GetConnectionString()))
             {
                 return cnn.Execute(sql, data);
             }
