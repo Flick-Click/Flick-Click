d10ff2d9897d15557a229b0473a269c171f492b3
William Sundgård Anker <will1331@edu.sde.dk>
2021-02-10 13:31:14 +0100

    Made CommentModel Contact v0.2 model done MovieDetails Done

---
+++ Flick Click/Models/CommentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Flick_Click.Models
+{
+    public class CommentModel
+    {
+        [Display(Name = "Comment")]
+        [StringLength(1000, MinimumLength = 10, ErrorMessage = "Write an actual sentence")]
+        [Required(ErrorMessage = "if you want to write a comment atleast write something")]
+        public string Comment { get; set; }
+    }
+}
