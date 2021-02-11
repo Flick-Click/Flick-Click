ba0d590d07d6147de9df52c6f84a61ccc1d61a3b
Faykier <elizabethroos47@gmail.com>
2021-02-11 12:10:52 +0100

    V0.1Search Start on Search function

--- Flick Click/Controllers/SearchController.cs
+++ Flick Click/Controllers/SearchController.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using static FlickClick_ClassLibary.BusinessLogic.SearchProcess;
+using Flick_Click.Models;
 using System.Web.Mvc;
 
 namespace Flick_Click.Controllers
 {
     public class SearchController : Controller
     {
+        [HttpPost]
         // GET: Seach
-        public ActionResult Search()
+        public ActionResult Search(string GSearch)
         {
             return View();
+            var data = LoadMovies(GSearch);
+            string Search = GSearch;
+
+            foreach(var row in data)
         }
     }
 }
---
+++ FlickClick_ClassLibary/BusinessLogic/SearchProcess.cs
@@ -0,0 +1,20 @@
+﻿using FlickClick_ClassLibary.DataAccess;
+using FlickClick_ClassLibary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlickClick_ClassLibary.BusinessLogic
+{
+    public class SearchProcess
+    {
+        public static List<MovieModel> LoadMovies(string GSearch)
+        {
+            string sql = $"SELECT * FROM movie WHERE title LIKE %{GSearch}%;";
+
+            return SqlDataAccess.LoadData<MovieModel>(sql);
+        }
+    }
+}
