46a7f7ac9c8d6c903b0be59543990035c8eb8297
Faykier <elizabethroos47@gmail.com>
2021-02-17 11:15:14 +0100

    V0.3SignIn Attempting to pass data

---
+++ Flick Click/Models/SignedInModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Flick_Click.Models
+{
+    public class SignedInModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
