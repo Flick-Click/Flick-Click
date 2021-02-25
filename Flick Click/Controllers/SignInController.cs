a349fa64e79e16aa03374040a68b9815b347513d
Tim Sommerstedt <timx1167@edu.sde.dk>
2021-02-25 08:45:49 +0100

    Security updates for sign in & Password hash on user creation.

--- Flick Click/Controllers/SignInController.cs
+++ Flick Click/Controllers/SignInController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using static FlickClick_ClassLibary.BusinessLogic.UserProcess;
+using static Flick_Click.Crypto;
 using Flick_Click.Models;
 using System.Web.Mvc;
 using System.IO;
@@ -21,7 +22,7 @@ namespace Flick_Click.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult SignInValidation(UserModel model)
         {
-            var data = SignInValidator(model.EmailAddress, model.Password);
+            var data = SignInValidator(model.EmailAddress, Hash(model.Password));
 
             // Handles wrong username and password
             if (data != null && !data.Any())
@@ -44,91 +45,123 @@ namespace Flick_Click.Controllers
 
         public ActionResult Logout()
         {
-            Session.Clear();
-            Session.Abandon();
+            if (Session["userID"] != null)
+            {
+                Session.Clear();
+                Session.Abandon();
+            }
             return RedirectToAction("Index", "Home");
         }
 
         // GET: SignIn
         public ActionResult ViewUser(int id)
         {
-            var data = LoadUser(id);
-            UserModel user = new UserModel();
-
-            foreach (var row in data)
+            if (Session["userID"] != null)
             {
-                user.ID = row.ID;
-                user.FirstName = row.FirstName;
-                user.LastName = row.LastName;
-                user.EmailAddress = row.Email;
-                user.TelefonNummer = row.TlfNr;
-                user.Group = row.Group;
-                user.Img = row.ProfilePicture;
-                user.Created = row.Created;
+                if (Convert.ToInt32(Session["userID"].ToString()) == id)
+                {
+                    var data = LoadUser(id);
+                    UserModel user = new UserModel();
+
+                    foreach (var row in data)
+                    {
+                        user.ID = row.ID;
+                        user.FirstName = row.FirstName;
+                        user.LastName = row.LastName;
+                        user.EmailAddress = row.Email;
+                        user.TelefonNummer = row.TlfNr;
+                        user.Group = row.Group;
+                        user.Img = row.ProfilePicture;
+                        user.Created = row.Created;
+                    }
+
+                    return View(user);
+                }
             }
 
-            return View(user);
+            return RedirectToAction("Index", "Home");
         }
 
         // GET: Edit User
         public ActionResult EditUser(int id)
         {
-            var data = LoadUser(id);
-            UserModel user = new UserModel();
-
-            foreach (var row in data)
+            if (Session["userID"] != null)
             {
-                user.ID = row.ID;
-                user.FirstName = row.FirstName;
-                user.LastName = row.LastName;
-                user.EmailAddress = row.Email;
-                user.TelefonNummer = row.TlfNr;
-                user.Img = row.ProfilePicture;
-            }
+                if (Convert.ToInt32(Session["userID"].ToString()) == id)
+                {
+                    var data = LoadUser(id);
+                    UserModel user = new UserModel();
 
-            return View(user);
+                    foreach (var row in data)
+                    {
+                        user.ID = row.ID;
+                        user.FirstName = row.FirstName;
+                        user.LastName = row.LastName;
+                        user.EmailAddress = row.Email;
+                        user.TelefonNummer = row.TlfNr;
+                        user.Img = row.ProfilePicture;
+                    }
+
+                    return View(user);
+                }
+            }
+            return RedirectToAction("Index", "Home");
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult EditUser(UserModel model)
         {
-            // Checks if there is an image uploaded
-            if (model.ImageFile != null)
+            if (Session["userID"] != null)
             {
-                // Get file name
-                string fileName = Path.GetFileNameWithoutExtension(model.ImageFile.FileName);
-                // Get file extension like jpg, gif etc.
-                string extension = Path.GetExtension(model.ImageFile.FileName);
-                // Set date on file name so no duplicates.
-                fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
-                // Set path
-                model.Img = "/Content/Pictures/" + fileName;
-                fileName = Path.Combine(Server.MapPath("/Content/Pictures/"), fileName);
-                // Save image
-                model.ImageFile.SaveAs(fileName);
-            }
+                if (Convert.ToInt32(Session["userID"].ToString()) == model.ID)
+                {
+                    // Checks if there is an image uploaded
+                    if (model.ImageFile != null)
+                    {
+                        // Get file name
+                        string fileName = Path.GetFileNameWithoutExtension(model.ImageFile.FileName);
+                        // Get file extension like jpg, gif etc.
+                        string extension = Path.GetExtension(model.ImageFile.FileName);
+                        // Set date on file name so no duplicates.
+                        fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                        // Set path
+                        model.Img = "/Content/Pictures/" + fileName;
+                        fileName = Path.Combine(Server.MapPath("/Content/Pictures/"), fileName);
+                        // Save image
+                        model.ImageFile.SaveAs(fileName);
+                    }
 
-            UpdateUser(model.ID, model.FirstName, model.LastName, model.EmailAddress, model.TelefonNummer, model.Img);
-            return RedirectToAction($"../SignIn/ViewUser/{model.ID}");
+                    UpdateUser(model.ID, model.FirstName, model.LastName, model.EmailAddress, model.TelefonNummer, model.Img);
+                    return RedirectToAction($"../SignIn/ViewUser/{model.ID}");
+                }
+            }
+            return RedirectToAction("Index", "Home");
         }
 
         public ActionResult UserComments(Nullable<int> id)
         {
-            var data = LoadUsersComments(id);
-            List<UserCommetModel> commets = new List<UserCommetModel>();
-
-            foreach (var row in data)
+            if (Session["userID"] != null)
             {
-                commets.Add(new UserCommetModel
+                if (Convert.ToInt32(Session["userID"].ToString()) == id)
                 {
-                   Title = row.Title,
-                   Created = row.Created,
-                   Content = row.Content
-                });
+                    var data = LoadUsersComments(id);
+                    List<UserCommetModel> commets = new List<UserCommetModel>();
 
+                    foreach (var row in data)
+                    {
+                        commets.Add(new UserCommetModel
+                        {
+                            Title = row.Title,
+                            Created = row.Created,
+                            Content = row.Content
+                        });
+
+                    }
+                    return View(commets);
+                }
             }
-            return View(commets);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
